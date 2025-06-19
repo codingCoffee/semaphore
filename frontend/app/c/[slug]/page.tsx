1f4a137703741b0e654a6bb31d9158ceb7a33a01
Ameya Shenoy <shenoy.ameya@gmail.com>
2025-06-19 21:03:36 +0530

    Revert "chore: landing for chat not found"
    This reverts commit f37758f9c2b9a4bc641257d927972051e74795c3.

--- frontend/app/c/[slug]/page.tsx
+++ frontend/app/c/[slug]/page.tsx
@@ -7,10 +7,7 @@ import { useZero, useQuery } from "@rocicorp/zero/react";
 import { usePathname } from "next/navigation";
 import { BottomMessageTextArea } from "@/components/BottomMessageTextArea";
 
-import { Sparkles } from "lucide-react";
 import MarkdownRenderer from "@/components/MarkdownRenderer";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
 
 export default function ChatPage() {
   const pathname = usePathname();
@@ -30,54 +27,26 @@ export default function ChatPage() {
     }
   }, [LLMResponseInstances]);
 
-  console.log("LLMResponseInstances:", LLMResponseInstances)
-
   return (
     <div className="flex flex-1 flex-col justify-end items-center pt-10 pb-40">
-      {LLMResponseInstances.length > 0 ? (
-        <>
-          <div className="md:w-[786px] w-[95%]">
-            {LLMResponseInstances.map((item) => (
-              <div key={item.id}>
-                <div className="flex justify-end">
-                  <div className="dark:bg-red-900 bg-red-200 p-2 rounded-lg markdown">
-                    <MarkdownRenderer markdown={item.question} />
-                  </div>
-                </div>
-                <div className="p-5 pt-10 rounded-lg markdown">
-                  <MarkdownRenderer markdown={item.answer ? item.answer : ""} />
-                </div>
+      <div className="md:w-[786px] w-[95%]">
+        {LLMResponseInstances.map((item) => (
+          <div key={item.id}>
+            <div className="flex justify-end">
+              <div className="dark:bg-red-900 bg-red-200 p-2 rounded-lg markdown">
+                <MarkdownRenderer markdown={item.question} />
               </div>
-            ))}
-            {/* This div will act as the scroll target */}
-            <div ref={endOfMessagesRef} />
+            </div>
+            <div className="p-5 pt-10 rounded-lg markdown">
+              <MarkdownRenderer markdown={item.answer ? item.answer : ""} />
+            </div>
           </div>
+        ))}
+        {/* This div will act as the scroll target */}
+        <div ref={endOfMessagesRef} />
+      </div>
 
-          <BottomMessageTextArea endOfMessagesRef={endOfMessagesRef} />
-        </>
-      ) : (
-        <div className="flex flex-col h-full items-center space-y-2 mt-16">
-          <div className="p-3 rounded-full">
-            <Sparkles className="h-6 w-6" />
-          </div>
-          <h1 className="text-3xl font-semibold text-center">
-            This Chat Doesn't Exist
-          </h1>
-          <p className="text-gray-400 text-center max-w-lg">
-            I'm Semaphore, an AI assistant, designed to help answer questions,
-            provide insights, and facilitate communication seamlessly.
-            <br />
-            <br />
-            You can ask me anything, from drafting an email to details about my
-            creator!
-          </p>
-          <Link href="/">
-            <Button variant="outline" className="cursor-pointer">
-              Start a New Chat
-            </Button>
-          </Link>
-        </div>
-      )}
+      <BottomMessageTextArea endOfMessagesRef={endOfMessagesRef} />
     </div>
   );
 }
