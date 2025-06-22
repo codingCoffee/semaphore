db09ddf414afc91b8dcf132fe4a4467f1b618b1b
Ameya Shenoy <shenoy.ameya@gmail.com>
2025-06-22 15:57:17 +0530

    fix: link in link
    Signed-off-by: Ameya Shenoy <shenoy.ameya@gmail.com>

--- frontend/components/team.tsx
+++ frontend/components/team.tsx
@@ -18,50 +18,52 @@ import {
 export function Team() {
   return (
     <SidebarMenu>
-      <Link href="/">
-        <div className="flex gap-2">
+      <div className="flex gap-2">
+        <Link href="/">
           <div className="bg-red-200 dark:bg-red-900 text-black dark:text-white flex aspect-square size-8 items-center justify-center rounded-lg">
             <SquareDashedBottomCode className="size-4" />
           </div>
-          <div className="grid flex-1 text-left text-sm leading-tight">
+        </Link>
+        <div className="grid flex-1 text-left text-sm leading-tight">
+          <Link href="/">
             <span className="truncate font-medium">Semaphore Chat (Alpha)</span>
-            <HoverCard>
-              <HoverCardTrigger asChild>
-                <span className="truncate text-xs hover:underline">
-                  by @codingcoffee
-                </span>
-              </HoverCardTrigger>
-              <HoverCardContent className="w-80">
-                <div className="flex justify-between gap-4">
-                  <Avatar>
-                    <AvatarImage src="https://avatars.githubusercontent.com/u/13611153?v=4" />
-                    <AvatarFallback>VC</AvatarFallback>
-                  </Avatar>
-                  <div className="space-y-1">
-                    <h4 className="text-sm font-semibold">Ameya Shenoy</h4>
-                    <p className="text-sm">
-                      I write code sometimes. Other times I'm busy writing todo
-                      lists.
-                    </p>
-                    <div className="flex flex-col text-muted-foreground text-xs">
-                      <Link
-                        href="https://codingcoffee.dev"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        codingcoffee.dev
-                      </Link>
-                      <Link href="mailto:shenoy.ameya@gmail.com">
-                        shenoy.ameya@gmail.com
-                      </Link>
-                    </div>
+          </Link>
+          <HoverCard>
+            <HoverCardTrigger asChild>
+              <span className="truncate text-xs hover:underline">
+                by @codingcoffee
+              </span>
+            </HoverCardTrigger>
+            <HoverCardContent className="w-80">
+              <div className="flex justify-between gap-4">
+                <Avatar>
+                  <AvatarImage src="https://avatars.githubusercontent.com/u/13611153?v=4" />
+                  <AvatarFallback>VC</AvatarFallback>
+                </Avatar>
+                <div className="space-y-1">
+                  <h4 className="text-sm font-semibold">Ameya Shenoy</h4>
+                  <p className="text-sm">
+                    I write code sometimes. Other times I'm busy writing todo
+                    lists.
+                  </p>
+                  <div className="flex flex-col text-muted-foreground text-xs">
+                    <Link
+                      href="https://codingcoffee.dev"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      codingcoffee.dev
+                    </Link>
+                    <Link href="mailto:shenoy.ameya@gmail.com">
+                      shenoy.ameya@gmail.com
+                    </Link>
                   </div>
                 </div>
-              </HoverCardContent>
-            </HoverCard>
-          </div>
+              </div>
+            </HoverCardContent>
+          </HoverCard>
         </div>
-      </Link>
+      </div>
     </SidebarMenu>
   );
 }
