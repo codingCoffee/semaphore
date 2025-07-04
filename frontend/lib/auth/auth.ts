22826f60164eee819614365c9faa5a3d76c5b635
Ameya Shenoy <shenoy.ameya@gmail.com>
2025-07-05 01:55:03 +0530

    chore: remove prompt
    Signed-off-by: Ameya Shenoy <shenoy.ameya@gmail.com>

--- frontend/lib/auth/auth.ts
+++ frontend/lib/auth/auth.ts
@@ -42,7 +42,6 @@ export const auth = betterAuth({
   },
   socialProviders: {
     google: {
-      prompt: "select_account",
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     },
