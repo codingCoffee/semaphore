983425d6b97a3fe4b71f62a82f938e6de12a5ce3
Ameya Shenoy <shenoy.ameya@gmail.com>
2025-07-07 00:18:17 +0530

    chore: add sample envs
    Signed-off-by: Ameya Shenoy <shenoy.ameya@gmail.com>

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,17 +5,20 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["*"],
   },
-    async rewrites() {
-    return [
-      {
-        source: "/api/script.js",
-        destination: `${process.env.NEXT_PUBLIC_RYBBIT_HOST}/api/script.js`,
-      },
-      {
-        source: "/api/track",
-        destination: `${process.env.NEXT_PUBLIC_RYBBIT_HOST}/api/track`,
-      },
-    ]
+  async rewrites() {
+    if (process.env.NEXT_PUBLIC_RYBBIT_HOST) {
+      return [
+        {
+          source: "/api/script.js",
+          destination: `${process.env.NEXT_PUBLIC_RYBBIT_HOST}/api/script.js`,
+        },
+        {
+          source: "/api/track",
+          destination: `${process.env.NEXT_PUBLIC_RYBBIT_HOST}/api/track`,
+        },
+      ]
+    }
+    return [];
   },
 };
 
